464d12a24b8083dd62f96e2b500c776c53231d0e
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-19 19:17:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog 
-  def initialize(dog_name, dog_breed)
-    @name = name
+  def initialize(dog_name, dog_breed = "Mutt")
+    @name = dog_name
+    @breed = dog_breed
   end
   
   def name
